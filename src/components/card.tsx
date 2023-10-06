70ef3b60cba5f1d6ff194899403493e9c38de5ee
Alex <ajoshua14@icloud.com>
2023-10-06 16:51:27 -0700

    Slap together a rudimentary carousel

---
+++ src/components/card.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { FC } from 'react'
+import { cn } from '@/lib/utils'
+
+interface cardProps extends React.HTMLAttributes<HTMLDivElement> {
+  image: string,
+  title: string,
+  description?: string,
+}
+
+const Card: FC<cardProps> = ({ image, title, description, className }) => {
+  return (
+    <div className={cn("h-full grid grid-rows-[3fr_1fr] grid-cols-1 gap-4", className)}>
+      <div className="h-full relative">
+        <Image
+          src="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80"
+          alt="space image"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className=" leading-relaxed bg-yellow-700">
+        <p className="font-medium">
+          {title}
+        </p>
+        {!!description &&
+          <p>
+            {description}
+          </p>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/sections/work.tsx
+++ src/sections/work.tsx
@@ -1,15 +1,61 @@
-import { FC } from 'react'
+'use client'
+
+import Card from '@/components/card'
+import Link from 'next/link'
+import { FC, useRef, useState } from 'react'
+import { IoChevronBack, IoChevronForward } from 'react-icons/io5'
 
 interface workProps {
 
 }
 
 const Work: FC<workProps> = ({ }) => {
+  const [carouselPosition, setCarouselPosition] = useState(0);
+  const carouselItem = useRef(null)
+
+  const items = [1, 2, 3, 4, 5];
+  const itemWidth = 27; //rem
+  const trackWidth = items.length * itemWidth;
+
+  const next = () => {
+    setCarouselPosition((prev) => {
+      return (prev + 1) % items.length;
+    })
+  }
+
+  const previous = () => {
+    setCarouselPosition((prev) => {
+      let index = prev - 1;
+      return index < 0 ? items.length - 1 : index;
+      // return (prev - 1) % items.length
+    })
+  }
+
   return (
-    <section className="h-20 w-full border-2 border-teal-700">
-      <h1>
+    <section className="w-full max-w-screen section section-content flex flex-col gap-8 border-2 border-teal-700">
+      <h2 className="section-header">
         Work
-      </h1>
+      </h2>
+      <div className="h-[40vh] w-full lg:h-[50vh] relative">
+        <div className={`h-full w-fit flex gap-4 absolute track translate-x-[-${carouselPosition * 27}rem] transition-all`}>
+          {items.map((item) => (
+            <Card key={item} image="" title="Interstellar destination" description="unkown, Milky Way" className={`h-full w-[${itemWidth}rem]`} />
+          ))}
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <Link href="/" className="link">
+          All Work
+        </Link>
+        <div className="flex items-center justify-between gap-6">
+          <button onClick={previous}>
+            <IoChevronBack size={20} />
+          </button>
+          <button onClick={next}>
+            <IoChevronForward size={20} />
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
