8c5285a1d2a0e9430f8e03fc4c648c900e977b38
Alex <ajoshua14@icloud.com>
2023-10-10 19:26:10 -0700

    Extract data into separate files

---
+++ src/data/recognition.ts
@@ -0,0 +1,40 @@
+import { GiSpaceNeedle, GiSpaceSuit, GiDustCloud } from "react-icons/gi";
+import { RiSpaceShipLine } from "react-icons/ri";
+
+export const recognizers = [
+  {
+    id: `126378`,
+    icon: RiSpaceShipLine,
+    iconSize: 40,
+    text: `Nebula Radiocast: Space-time defying ships`,
+    link: `/`,
+  },
+  {
+    id: `1232898`,
+    icon: GiSpaceNeedle,
+    iconSize: 40,
+    text: `Intestellar Relays: Exquisite execution on warp engines`,
+    link: `/`,
+  },
+  {
+    id: `19808321`,
+    icon: GiSpaceSuit,
+    iconSize: 40,
+    text: `Journeymen of Space: "We couldn't believe our eyes when we first saw.."`,
+    link: `/`,
+  },
+  {
+    id: `098328`,
+    icon: RiSpaceShipLine,
+    iconSize: 40,
+    text: `Nebula Radiocast: Space-time defying ships`,
+    link: `/`,
+  },
+  {
+    id: `0203283`,
+    icon: GiDustCloud,
+    iconSize: 40,
+    text: `Nebula Radiocast: Space-time defying ships`,
+    link: `/`,
+  },
+]
